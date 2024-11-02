360153748f0ef6f2d2de222e9aa41f01aa95a76b
Michael Gill <66725656+gillworks@users.noreply.github.com>
2024-11-02 19:53:39 -0400

    add supabase for player auth and location

---
+++ config.js
@@ -0,0 +1,5 @@
+window.env = {
+  NEXT_PUBLIC_SUPABASE_URL: "https://wosuhfhlfjhnuxtcwuje.supabase.co",
+  NEXT_PUBLIC_SUPABASE_ANON_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvc3VoZmhsZmpobnV4dGN3dWplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA1ODg3MzIsImV4cCI6MjA0NjE2NDczMn0.Ge20RJBFaRKijrUDxzxl92ftBiTRjv5a_lN6amhDKbY",
+};
